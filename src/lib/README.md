1a5f000fde3a6c845f29af2c7f427c136ebe9210
jindrich.samec <samec@brandembassy.com>
2016-02-09 22:51:15 +0100

    project reset

--- src/lib/README.md
+++
@@ -1,2 +0,0 @@
-As you are writing app, you realize some code should be written reusable. Put it
-into lib directory, and maybe publish to npm later.
--- src/server/api/index.js
+++
@@ -1,9 +0,0 @@
-import generator from 'password-generator'
-
-export default {
-
-	handle: (req, res) => {
-		res.send({gameId: generator(6, true, '[0-9][A-Z]')});
-	}
-
-}
